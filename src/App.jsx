a29ae562bf971c88c1458d785f8b1cd61093bc29
lolster45 <oscarneutron3@gmail.com>
2024-09-09 12:39:04 -0500

    add footer and color style changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ import Contact from './pages/Contact'
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
 
 
 
@@ -22,17 +25,25 @@ import './App.scss'
 function App() {
 
   return (
-    <div className="App">
+    <div className="App poppins-light">
       <HashRouter>
         
           <div className='nav-bar'>
             <h3>Royalty Solutions</h3>
             <nav className='nav-wrap'>
-              <Link to={'/'}>Home</Link>
-              <Link to={'/products'}>Products</Link>
-              <Link to={'/contact'}>Contact</Link>
+              <span>
+                <Link to={'/'}>Home</Link>      
+              </span>
+              <span>
+                <Link to={'/products'}>Products</Link>
+              </span>
+              <span>
+                <Link to={'/contact'}>Contact</Link>
+              </span>
             </nav>
             
+
+
             <div className='social-media-links'>
               <FaFacebook/>
               <FaInstagram/>
@@ -48,10 +59,25 @@ function App() {
               <Route path="/" element={<Home/>}/>
               <Route path="/products" element={<ProductsList/>}/>
               <Route path="/contact" element={<Contact/>}/>
-            
-
-
           </Routes>
+
+
+          <footer>
+
+            <div>
+              <h2>Royalty Solutions</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sapiente laudantium, rem dolore architecto sint praesentium ratione officiis autem fuga nobis tenetur a neque iure itaque quia dicta necessitatibus! Ipsum?</p>
+              <div className='social-icons-footer'>
+                <FaFacebook/>
+                <FaInstagram/>
+                <FaTiktok/>
+                <FaGoogle/>
+                <FaTwitter/>
+              </div>
+            </div>
+
+          </footer>
+
       </HashRouter>
     </div>
   )
