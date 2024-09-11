bb1ceb078776193947ca318594c637ff00590dc1
lolster45 <oscarneutron3@gmail.com>
2024-09-10 21:29:52 -0500

    product js file

---
+++ src/products.js
@@ -0,0 +1,111 @@
+
+
+const products = [
+    {
+        id: "fasl832n",
+        name: "StateBoard kitt",
+        price: 100,
+        productImage: "https://stateboardprofessor.com/wp-content/uploads/2020/12/IMG_1305-1-scaled.jpg",
+        description: "All the essentials needed",
+        reviews: [
+            {
+                name: "Alma",
+                profileURL: "",
+                description: "Amazing product! It prepared me!",
+                rating: "5",
+            }
+        ]
+    },
+    {
+        id: "238fasifsa",
+        name: "Mannequin Head",
+        price: 20,
+        productImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi2xVWO6-i_ZO5aK_q5NMDM34KfZuf-D8USQ&s",
+        description: "The head of a manequin so you can practice on",
+        reviews: [
+            {
+                name: "Alma",
+                profileURL: "",
+                description: "Amazing product! It prepared me!",
+                rating: "5",
+            }
+        ]
+    },
+    {
+        id: "3wmweioq",
+        name: "Scissors",
+        price: 100,
+        productImage: "https://m.media-amazon.com/images/I/71RDZCnC9bL.jpg",
+        description: "This is a pair of scissors that was once used by arthur",
+        reviews: [
+            {
+                name: "Goku",
+                profileURL: "",
+                description: "Amazing product! It prepared me!",
+                rating: "5",
+            }
+        ]
+    },
+    {
+        id: "19rwwgnkw",
+        name: "Water Sprayer",
+        price: 100,
+        productImage: "https://i5.walmartimages.com/asr/52b5bce6-cfd8-4567-98b2-25eaf0f8e783.dba85c023322eade2ab41cb0c31abb0a.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+        description: "Sprays water",
+        reviews: [
+            {
+                name: "Andre",
+                profileURL: "",
+                description: "Amazing product! It prepared me!",
+                rating: "5",
+            }
+        ]
+    },
+    {
+        id: "193inskf",
+        name: "Juice",
+        price: 100,
+        productImage: "https://stateboardprofessor.com/wp-content/uploads/2020/12/IMG_1305-1-scaled.jpg",
+        description: "",
+        reviews: [
+            {
+                name: "Alma",
+                profileURL: "",
+                description: "Amazing product! It prepared me!",
+                rating: "5",
+            }
+        ]
+    },
+    {
+        id: "193nskfsekf",
+        name: "Water Sprayer",
+        price: 100,
+        productImage: "https://i5.walmartimages.com/asr/52b5bce6-cfd8-4567-98b2-25eaf0f8e783.dba85c023322eade2ab41cb0c31abb0a.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+        description: "Sprays water",
+        reviews: [
+            {
+                name: "Andre",
+                profileURL: "",
+                description: "Amazing product! It prepared me!",
+                rating: "5",
+            }
+        ]
+    },
+    {
+        id: "10wekgnkwef",
+        name: "Juice",
+        price: 100,
+        productImage: "https://stateboardprofessor.com/wp-content/uploads/2020/12/IMG_1305-1-scaled.jpg",
+        description: "",
+        reviews: [
+            {
+                name: "Alma",
+                profileURL: "",
+                description: "Amazing product! It prepared me!",
+                rating: "5",
+            }
+        ]
+    }
+]
+
+export default products
